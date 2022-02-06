16612f47606be3a473163162fc713e87bb4c9d18
Maxine Ileto <xam.ileto@gmail.com>
2022-02-07 00:12:41 +0800

    added form and changed action in user-search hbs

--- public/js/small-box.js
+++ public/js/small-box.js
@@ -1,3 +1,19 @@
+function formAction() {
+  url = window.location.href;
+  form = $('#submit');
+
+  if (url.includes('ask')) {
+    form.attr('action', 'post/ask');
+  } else if (url.includes('edit-post')) {
+    form.attr('action', 'post/edit-post');
+  } else if (url.includes('edit-comment')) {
+    form.attr('action', 'post/edit-comment');
+  } else {
+    //if searching
+    form.attr('action', 'post/search');
+  }
+}
+
 $(document).ready(() => {
-  // formAction();
+  formAction();
 });
