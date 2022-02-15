f5eef466b8eaaf68dd6ab2e4bf5f1bcdaa423cda
Maxine Ileto <xam.ileto@gmail.com>
2022-02-16 07:06:59 +0800

    fixed index-logged-in post URLs

--- controller/view-controller.js
+++ controller/view-controller.js
@@ -1,4 +1,5 @@
 const postModel = require('../database/models/post');
+const accountModel = require('../database/models/account');
 const accountController = require('./account-controller.js');
 
 // for showing all posts in index-logged-in
@@ -6,15 +7,20 @@ exports.showAllPosts = async function (req, res) {
   posts = await postModel.getAllPosts();
   newPosts = [];
 
-  posts.forEach((element) => {
-    console.log;
+  for (element of posts) {
+    var accountNameId = await accountModel.findOneByAccountName(
+      element.accountName
+    );
+    accountNameId = accountNameId._id.toString();
+
     post = {
       postId: element._id.toString(),
       postTitle: element.title,
       accountName: element.accountName,
+      accountNameId: accountNameId,
     };
     newPosts.push(post);
-  });
+  }
 
   accountName = req.user.accountName;
   var data = {
@@ -23,6 +29,27 @@ exports.showAllPosts = async function (req, res) {
     isInIndex: true,
     accountName: accountName,
     posts: newPosts,
+    currentUser: req.user._id.toString(),
+    showAccountDetails: true,
+  };
+
+  res.render('index', data);
+};
+
+// for showing search results
+exports.showSearchPosts = async (req, res) => {
+  console.log(req.body.content);
+
+  postsWithQuery = await postModel.getByQuery(req.body.content);
+
+  console.log(postsWithQuery);
+
+  var data = {
+    isLoggedIn: true,
+    // for search icon
+    isInIndex: true,
+    accountName: req.user.accountName,
+    posts: postsWithQuery,
     currentUser: req.user._id.toString(),
     showAccountDetails: true,
   };
