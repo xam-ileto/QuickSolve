94ecacf764bcd4ac40f658d1866147c9a57a1c04
Maxine Ileto <xam.ileto@gmail.com>
2022-02-17 15:19:43 +0800

    removed comments

--- routes/account-router.js
+++ routes/account-router.js
@@ -1,7 +1,6 @@
 const express = require('express');
 var router = express.Router();
 
-// const Account = require('../database/models/Account');
 const accountController = require('../controller/account-controller');
 const authenticator = require('../authenticator.js');
 
@@ -26,44 +25,3 @@ router.get('/view/:id', accountController.showAccountPage);
 router.post('/delete', accountController.deleteAccount);
 
 module.exports = router;
-
-// router.get('/page', function (req, res) {
-//   var data = {
-//     isLoggedIn: true,
-//     // for search icon
-//     isInIndex: true,
-//     layout: 'account.hbs',
-//     accountName: 'NancyLandgraab',
-//   };
-//   res.render('index', data);
-// });
-
-// router.get('/delete', (req, res) => {
-//   name = 'test';
-// });
-
-// router.get('/modify-name', (req, res) => {
-//   oldName = 'test';
-//   newName = 'newtest';
-
-//   Account.findOneAndUpdate(
-//     { accountName: oldName },
-//     { accountName: newName },
-//     (error, post) => {
-//       res.redirect('/login');
-//     }
-//   );
-// });
-
-// router.get('/modify-password', (req, res) => {
-//   accountName = 'newtest';
-//   password = '5678';
-
-//   Account.findOneAndUpdate(
-//     { accountName: accountName },
-//     { password: password },
-//     (error, post) => {
-//       res.redirect('/login');
-//     }
-//   );
-// });
